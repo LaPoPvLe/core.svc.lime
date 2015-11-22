307d78cfc036b526c8fc3a0587eb83a37a2d2fef
LaPoPvLe <lingfeng8250@163.com>
2015-11-22 18:47:42 +0800

    cehsi

---
+++ js/app.js
@@ -0,0 +1,13 @@
+/**
+ * Created by qinghai<qinghai@zgpgc.com> on 2015/11/22.
+ * @copyright 2015 - HCLC
+ * @link http://www.zgpgc.com
+ */
+
+import Koa from 'koa';
+
+class App {
+
+}
+
+export default App;
